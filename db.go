29fda8412ba4d11a89e3036d98cd1ebb3bc1f851
neerajsingh00 <neeraj.singh@hipbar.com>
2019-10-25 15:00:48 +0530

    external_functions

--- db.go
+++ db.go
@@ -16,17 +16,19 @@ const (
 )
 
 
+var psqlInfo = fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+var db, err = sql.Open("postgres", psqlInfo)
+
+
 //function to check errors
 func checkErr(err error) {
 	if err != nil {
 		panic(err)
    }
 }
-
+	
 //establishing connection with the PSQL server
 func init() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
 	checkErr(err)
   	err = db.Ping()
   	checkErr(err)
@@ -36,47 +38,7 @@ func init() {
 }
 
 
-/*func update() {
-	sqlStatement := `UPDATE profile_schema.profile SET  kyc_level = $2 WHERE consumer_id = $1 RETURNING consumer_id ;`
-    	
-	var email string
-	var id int
-	
-	err = db.QueryRow(sqlStatement, 101, 2).Scan(&consumer_id, &kyc_level)
-	
-	checkErr(err)
-	
-	fmt.Println(consumer_id, kyc_level)
-
-}*/	
-
-//fetching data from the database
-/*func fetch() {
-	db, err := sql.Open("postgres", psqlInfo)
-	rows, err := db.Query("SELECT * FROM profile_schema.profile")   
-    checkErr(err)
-	for rows.Next() {
-		var consumer_id, kyc_level int
-        var gender,sign_up_date *string	
-        err = rows.Scan(&consumer_id, &kyc_level, &gender, &sign_up_date)
-        checkErr(err)
-        fmt.Println("consumer_id | kyc_level | gender | sign_up_date ")
-        fmt.Printf("%11v | %9v | %6v | %12v\n", consumer_id, kyc_level, gender, sign_up_date)
-	}
-} */
-
-
-func main() {
-
-	//establishing connection
-	fmt.Println("")
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
-	checkErr(err)
-  	err = db.Ping()
-  	checkErr(err)
-
-	//querying data
+func fetch() {
 	fmt.Println("------------querying----------")
 	rows, err := db.Query("SELECT * FROM profile_schema.profile")   
     checkErr(err)
@@ -89,23 +51,38 @@ func main() {
         checkErr(err)
         fmt.Printf("%11v | %6v | %9v | %12v\n", consumer_id, gender, kyc_level, sign_up_date)
 	}
-	
-	//inserting values
+
+
+}
+
+func insert() {
 
 	var lastInsertId int
-    err = db.QueryRow("INSERT INTO profile_schema.profile(consumer_id, gender, kyc_level ) VALUES ($1,$2,$3);",1,"female",2).Scan(&lastInsertId)
+    err = db.QueryRow("INSERT INTO profile_schema.profile(consumer_id, gender, kyc_level ) VALUES ($1,$2,$3);",5,"f",2).Scan(&lastInsertId)
 
-	//updating values
+}
+
+func update() {
+
 	sqlStatement := `UPDATE profile_schema.profile SET kyc_level = $2 WHERE consumer_id = $1;`
 	
-	_, err = db.Exec(sqlStatement, 100, 2)
-	
-	if err != nil {
-		panic(err)
-	}
+	_, err = db.Exec(sqlStatement, 5, 1)
+	checkErr(err)	
+		
+
+} 
 
 
+func main() {
 
-
+	fetch()
+	fmt.Println("----------------")
+	insert()
+	fetch()
+	fmt.Println("-----------------")
+	update()
+	fetch()
+	fmt.Println("--------------------")
 	defer db.Close()
+
 }
