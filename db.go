b01e0232757c7501f1655c66dcd472d8859cd62d
neerajsingh00 <neeraj.singh@hipbar.com>
2019-10-30 16:05:43 +0530

    alter added

--- db.go
+++ db.go
@@ -3,9 +3,17 @@ package main
 import (
   "database/sql"
   "fmt"
-
-  _ "github.com/lib/pq"
+  "github.com/lib/pq"
 )
+
+
+type profile struct {
+	consumer_id int
+	gender string
+	kyc_level int 
+	sign_up_date int
+
+}
 
 const (
   host     = "localhost"
@@ -18,12 +26,12 @@ const (
 
 var psqlInfo = fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 var db, err = sql.Open("postgres", psqlInfo)
-
+var pro = make([]*profile, 0)
 
 //function to check errors
 func checkErr(err error) {
 	if err != nil {
-		panic(err)
+		panic(err)	
    }
 }
 	
@@ -42,17 +50,26 @@ func fetch() {
 	fmt.Println("------------querying----------")
 	rows, err := db.Query("SELECT * FROM profile_schema.profile")   
     checkErr(err)
-	fmt.Println("consumer_id | gender | kyc_level | sign_up_date | HipBar ")
+	fmt.Println("consumer_id | gender | kyc_level | sign_up_date ")
     
 	for rows.Next() {
-		var consumer_id, kyc_level int
-        var gender,sign_up_date,hipbar *string	
-        err = rows.Scan(&consumer_id, &gender, &kyc_level, &sign_up_date,&hipbar)
+		p := new(profile)	
+        err = rows.Scan(&p.consumer_id, &p.gender, &p.kyc_level, &p.sign_up_date)
         checkErr(err)
-        fmt.Printf("%11v | %6v | %9v | %12v | %6v\n", consumer_id, gender, kyc_level, sign_up_date, hipbar)
+		pro = append(pro,p)
+    }
+	
+	if err = rows.Err(); err != nil {
+		panic(err)
 	}
+	
+	for _, p := range pro {
+		fmt.Println ("%12v, %6v, %9v, %12v \n",&p.consumer_id, &p.gender, &p.kyc_level, &p.sign_up_date)
+	}
+	
 
 
+	defer rows.Close()
 }
 
 //function to insert values
@@ -72,22 +89,33 @@ func update() {
 	checkErr(err)	
 } 
 
-//function to add/drop column
-func alter() {
+//function to add column
+func alter_column() {
+	sqlStatement := `ALTER TABLE profile_schema.profile ADD COLUMN hipbar int ;`
+	_, err = db.Exec(sqlStatement)
+	checkErr(err)
+}
+
+//function to rename column
+func alter_rename_column() {
+	sqlStatement := `ALTER TABLE profile_schema.profile RENAME COLUMN hipbar TO hipster_bar ;`
+	_, err = db.Exec(sqlStatement)
+	checkErr(err)
+}
+
+//function to drop a column
+func alter_drop_column() {
+	sqlStatement := `ALTER TABLE profile_schema.profile DROP COLUMN hipster_bar ;`
+	_, err = db.Exec(sqlStatement)
+	checkErr(err)
 
 }
 
-
 func main() {
 
+	
 	fetch()
-	fmt.Println("----------------")
-	insert()
-	fetch()
-	fmt.Println("-----------------")
-	update()
-	fetch()
-	fmt.Println("--------------------")
+	fmt.Println("-------")
 	defer db.Close()
 
 }
