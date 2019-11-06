e0493a9fa72aeaa9ac96feb3d51e30d6f58cef81
neerajsingh00 <neeraj.singh@hipbar.com>
2019-11-06 14:42:59 +0530

    insert func optimized

--- db.go
+++ db.go
@@ -34,6 +34,42 @@ var psqlInfo = fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s ssl
 var db, err = sql.Open("postgres", psqlInfo)
 var pro = make([]*profile, 0)
 
+//establishing connection with the PSQL server
+func init() {
+	checkErr(err)
+  	err = db.Ping()
+  	checkErr(err)
+
+  	fmt.Println("Connection Established!")
+	fmt.Println("------------------------")
+}
+
+//function to query 
+func fetch() {
+	fmt.Println("------------querying----------")
+	rows, err := db.Query("SELECT * FROM profile_schema.profile")   
+    checkErr(err)
+	println("")
+	fmt.Println("consumer_id | gender | kyc_level | sign_up_date ")
+    
+	for rows.Next() {
+		p := new(profile)	
+        err = rows.Scan(&p.consumer_id, &p.gender, &p.kyc_level, &p.sign_up_date)
+        checkErr(err)
+		pro = append(pro,p)
+    }
+	
+	if err = rows.Err(); err != nil {
+		panic(err)
+	}
+	
+	for _, p := range pro {
+		fmt.Println (p.consumer_id,"\t |",p.gender,"\t |", p.kyc_level,"\t |", p.sign_up_date)
+	}
+	
+	defer rows.Close()
+}
+
 //function to check errors 
 func checkErr(err error) {
 	if err != nil {
@@ -58,48 +94,14 @@ func create_table() {
 	println("----table created----")
 }
 
-
-//establishing connection with the PSQL server
-func init() {
-	checkErr(err)
-  	err = db.Ping()
-  	checkErr(err)
-
-  	fmt.Println("Connection Established!")
-	fmt.Println("------------------------")
-}
-
-//function to query 
-func fetch() {
-	fmt.Println("------------querying----------")
-	rows, err := db.Query("SELECT * FROM profile_schema.profile")   
-    checkErr(err)
-	fmt.Println("consumer_id | gender | kyc_level | sign_up_date ")
-    
-	for rows.Next() {
-		p := new(profile)	
-        err = rows.Scan(&p.consumer_id, &p.gender, &p.kyc_level, &p.sign_up_date)
-        checkErr(err)
-		pro = append(pro,p)
-    }
-	
-	if err = rows.Err(); err != nil {
-		panic(err)
-	}
-	
-	for _, p := range pro {
-		fmt.Println (p.consumer_id,"\t |",p.gender,"\t |", p.kyc_level,"\t |", p.sign_up_date)
-	}
-	
-	defer rows.Close()
-}
-
 //function to insert values
 func insert() {
+	
+	sqlStatement := `INSERT INTO profile_schema.profile(consumer_id, gender, kyc_level) VALUES ($1,$2,$3)`
 
-	var lastInsertId int
-	err = db.QueryRow("INSERT INTO profile_schema.profile(consumer_id, gender, kyc_level, hipbar ) VALUES ($1,$2,$3,$4);",5,"f",2,"beer").Scan(&lastInsertId)
-	err = db.QueryRow("INSERT INTO profile_schema.profile(consumer_id,gender,kyc_level,hipbar) VALUES ($1,$2,$3,$4);",6,"f",0,"beer").Scan(&lastInsertId)
+	_, err = db.Exec(sqlStatement,103,"female",2)
+	checkErr(err)
+
 	println("----inserted values----")
 }
 
@@ -151,7 +153,7 @@ func alter_and_update_column() {
 
 func main() {
 
-	create_table()
+	fetch()	
 	defer db.Close()
 
 }
